d006b4580f01504f04001b0d8211a326639b3fc8
metabynull <bynull@metaelon.space>
2022-07-19 04:39:55 -0400

    Gh actions: add docker deployment. Fix cmd

--- builder.sh
+++ builder.sh
@@ -10,4 +10,4 @@
 docker build -t meta-secret-builder --file Dockerfile-builder .
 docker run -ti --rm -v $(pwd)/target:/src/target meta-secret-builder
 
-docker build -t ghcr.io/meta-secret-cli:latest --file Dockerfile-cli
+docker build -t ghcr.io/meta-secret/meta-secret-cli:latest --file Dockerfile-cli .
