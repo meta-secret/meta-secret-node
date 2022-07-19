6c7929a86816867f970099e450fcc9c24b905977
metabynull <bynull@metaelon.space>
2022-07-19 12:08:24 -0400

    Cli docker deployment: get rid of tty

--- builder.sh
+++ builder.sh
@@ -10,13 +10,5 @@ set -e
 # - https://dev.to/rogertorres/first-steps-with-docker-rust-30oi
 # - github issue https://github.com/rust-lang/cargo/issues/2644
 docker build -t meta-secret-builder --file Dockerfile-builder .
-
-echo execute cargo build
-docker run -ti --rm -v $(pwd)/target:/src/target meta-secret-builder
-
-echo pwd: $(pwd)
-
-echo list curr directory
-ls -la
-
+docker run --rm -v $(pwd)/target:/src/target meta-secret-builder
 docker build -t ghcr.io/meta-secret/meta-secret-cli:latest --file Dockerfile-cli .
