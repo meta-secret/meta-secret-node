b0ad10bc9e9dc7b1c19d2447ff71ca1236e58a38
metabynull <bynull@metaelon.space>
2022-07-19 12:04:52 -0400

    Cli docker deployment: check params

--- builder.sh
+++ builder.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Building rust app using docker multi-build is super slow.
 # We need an optimization that pre builds all the dependencies in a target dir and then
 # the cli docker image can use meta-secret-cli app.
@@ -8,6 +10,13 @@
 # - https://dev.to/rogertorres/first-steps-with-docker-rust-30oi
 # - github issue https://github.com/rust-lang/cargo/issues/2644
 docker build -t meta-secret-builder --file Dockerfile-builder .
+
+echo execute cargo build
 docker run -ti --rm -v $(pwd)/target:/src/target meta-secret-builder
+
+echo pwd: $(pwd)
+
+echo list curr directory
+ls -la
 
 docker build -t ghcr.io/meta-secret/meta-secret-cli:latest --file Dockerfile-cli .
