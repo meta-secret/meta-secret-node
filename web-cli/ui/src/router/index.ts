906049378ef0f9f061eb63112f90139150be009a
metabynull <bynull@metaelon.space>
2022-08-03 20:24:02 -0400

    web-cli: 404 view

--- web-cli/ui/src/router/index.ts
+++ web-cli/ui/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import SplitView from "../views/SplitView.vue";
 import RecoverView from "../views/RecoverView.vue";
 import ContactView from "../views/ContactView.vue";
+import NotFoundView from "../views/404View.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,15 @@ const router = createRouter({
             name: "contact",
             component: ContactView,
         },
+        {
+            path: '/404',
+            name: 'PageNotExist',
+            component: NotFoundView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            redirect: "/404"
+        }
     ],
 });
 
