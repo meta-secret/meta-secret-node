f67f4d1343663ab7eca315befa02c83245ed3673
metabynull <bynull@meta-secret.org>
2022-08-30 23:11:18 -0700

    core, mobile: building swift package, part 3

--- mobile/core-swift-lib/Package.swift
+++ mobile/core-swift-lib/Package.swift
@@ -2,25 +2,12 @@
 import PackageDescription
 import Foundation
 let package = Package(
-        name: "MetaSecretCoreLib",
-        platforms: [
-            .iOS(.v13),
-        ],
-        products: [
-            .library(
-                name: "MetaSecretCoreLib",
-                targets: ["MetaSecretCoreLib"]),
-        ],
-        targets: [
-            .target(
-                name: "MetaSecretCoreLib",
-                dependencies: ["MetaSecretCore"]),
-            .binaryTarget(
-                name: "MetaSecretCore",
-                url: "https://github.com/...../bundle.zip",
-                checksum: "ea7a.....35b2"),
-            .testTarget(
-                name: "RustToSwiftTests",
-                dependencies: ["RustToSwift"]),
-        ]
+    name: "MetaSecretCore",
+    platforms: [
+        .iOS(.v13),
+    ],
+    targets: [
+        .systemLibrary(name: "metasecretcore"),
+        .target(name: "MetaSecretCoreLib", dependencies: ["metasecretcore"])
+    ]
 )
