3a00bb76e597038ba2b41c07d5211b450f323ce6
metabynull <bynull@meta-secret.org>
2023-01-07 22:20:35 -0800

    fix gh actions: fix actions and generate schema models
    Signed-off-by: metabynull <bynull@meta-secret.org>

--- web-cli/build.sh
+++
@@ -1,4 +0,0 @@
-
-docker run -ti --rm -v "$(pwd)/wasm":/app meta-secret/web-cli-builder:latest sh -c "wasm-pack build --target web --dev"
-rm -rf ui/pkg
-cp -R wasm/pkg ui
--- web-cli/builder.sh
+++
@@ -1,6 +0,0 @@
-
-rm -rf target
-mkdir -p target/core
-cp -R ../core target
-docker build -t meta-secret/web-cli-builder:latest --file Dockerfile-builder .
-
